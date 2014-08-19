7c045ad6d4de551d79bbd4dcd0a3f6bcff484aae
Flavio Coutinho <fegemo@gmail.com>
2014-08-19 16:49:24 -0300

    :lollipop: Initial commit

---
+++ src/index.md
@@ -0,0 +1,34 @@
+# Aula do CEFET
+*Professor:* Flávio Coutinho
+
+---
+
+# Instruções de uso (1)
+
+  1. Instale o git e o nodejs
+  1. "Fork" [este repositório][fork] no GitHub
+  1. Instale o gulp
+
+    ```bash
+    $ npm install gulp -g
+    ```
+[fork]: http://github.com/fegemo/class-template
+
+---
+
+# Instruções de uso (2)
+
+  1. Em linha de comando, inicie a apresentação
+
+    ```bash
+    $ gulp build serve
+    ```
+    Seu navegador abrirá mostrando este modelo de apresentação
+  1. Edite os arquivos e faça sua apresentação
+    - `src/index.html`
+    - `src/index.md`
+    - `src/styles/main.styl`
+    - `src/images`
+    - `src/scripts/main.js`
+
+---
---
+++ src/scripts/main.js
@@ -0,0 +1,24 @@
+// Require Node modules in the browser thanks to Browserify: http://browserify.org
+var bespoke = require('bespoke'),
+    fancy = require('bespoke-theme-fancy'),
+    keys = require('bespoke-keys'),
+    touch = require('bespoke-touch'),
+    bullets = require('bespoke-bullets'),
+    scale = require('bespoke-scale'),
+    hash = require('bespoke-hash'),
+    progress = require('bespoke-progress'),
+    state = require('bespoke-state'),
+    markdown = require('bespoke-markdown');
+
+// Bespoke.js
+bespoke.from('article', [
+  markdown(),
+  fancy(),
+  keys(),
+  touch(),
+  bullets('li, .bullet'),
+  scale(),
+  hash(),
+  progress(),
+  state()
+]);
